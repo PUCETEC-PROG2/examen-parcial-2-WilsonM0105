e4294350da86fab5d96dbb4fe3f560c26e9c9136
WilsonMites <wilsonmites1@gmail.com>
2025-01-11 10:45:13 -0500

    Parcial 2 Completado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    year = models.IntegerField()
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 
-# Create your views here.
+
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, 'index.html', {'movies': movies})
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'movie_detail.html', {'movie': movie})
